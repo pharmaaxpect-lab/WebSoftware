99fe9a1455ce11bb3a6cf65bb8e57519b195baeb
pharmaaxpect-lab <pharmaaxpect@gmail.com>
2025-09-23 16:04:43 +0530

    Fix dotenv require + mongoose setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const dotenv = require('dotenv');
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("✅ Backend is running!");
