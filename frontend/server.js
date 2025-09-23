f628c34740d4a910803ca3425271e9dd11ebe3ae
pharmaaxpect-lab <pharmaaxpect@gmail.com>
2025-09-23 16:57:38 +0530

    Added frontend views, public assets, and updated package.json

--- frontend/server.js
+++ frontend/server.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("<h1>✅ Frontend is running!</h1>");
-});
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
+
+// set ejs as view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+// =========== Login Home Page ======================
+
+app.get("/", (req, res) => {
+  res.render("login"); // views/login.ejs
+});
+
+// app.get("/", (req, res) => {
+//   res.send("<h1>✅ Frontend is running!</h1>");
+// });
+
 app.listen(PORT, () => console.log(`Frontend running on port ${PORT}`));
